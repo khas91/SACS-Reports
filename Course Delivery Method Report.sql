02835bc9cacb0eb08ae52a09d06e555d2fa8bda9
khas91 <khasquakhas@gmail.com>
2016-08-17 14:17:10 -0400

    BBasically started ove

--- Course Delivery Method Report.sql
+++ Course Delivery Method Report.sql
@@ -18,82 +18,122 @@
 */
 
 
-IF OBJECT_ID ('tempdb..#temp') IS NOT NULL
-	DROP TABLE #temp
-IF OBJECT_ID ('tempdb..#programs') IS NOT NULL
-	DROP TABLE #programs
-
+DECLARE @awd_type VARCHAR(MAX) = 'VC'
+DECLARE @pgm_cd VARCHAR(MAX) = ''
 DECLARE @min_term CHAR(5) = '20103'
 DECLARE @max_term CHAR(5) = '20153'
-DECLARE @awd_type VARCHAR(6) = 'VC'
-DECLARE @pgm VARCHAR(4) = ''
+
+
+IF OBJECT_ID('tempdb..#programs') IS NOT NULL
+	DROP TABLE #programs
+IF OBJECT_ID('tempdb..#programcourserequirements') IS NOT NULL
+	DROP TABLE #programcourserequirements
 
 CREATE TABLE #programs
 (
-	PGM_CD VARCHAR(MAX)
+	PGM_CD CHAR(4)
 	,AWD_TY VARCHAR(MAX)
+	,HRS_REQD VARCHAR(MAX)
 )
 
-IF (@awd_type = '')
+
+CREATE TABLE #programcourserequirements
+(
+	PGM_CD CHAR(4)
+	,CRS_ID VARCHAR(MAX)
+	,HRS VARCHAR(MAX)
+	,OfferedOnline VARCHAR(MAX)
+)
+
+IF (@pgm_cd <> '')
 BEGIN
+
 	INSERT INTO #programs
 		SELECT
 			prog.PGM_CD
 			,prog.AWD_TY
+			,prog.PGM_TTL_MIN_CNTCT_HRS_REQD
 		FROM
 			MIS.dbo.ST_PROGRAMS_A_136 prog
 		WHERE
-			prog.EFF_TRM_D <> ''
-			AND prog.PGM_CD = @pgm
-			AND prog.END_TRM = ''
+			prog.PGM_CD = @pgm_cd
+			AND prog.EFF_TRM_D <> ''
+			AND prog.EFF_TRM_D <= @max_term
+			AND prog.AWD_TY = @awd_type
+			AND (prog.END_TRM = '' OR prog.END_TRM >= @max_term)
 END
 ELSE IF (@awd_type <> '')
 BEGIN
+	
 	INSERT INTO #programs
 		SELECT
 			prog.PGM_CD
 			,prog.AWD_TY
+			,prog.PGM_TTL_MIN_CNTCT_HRS_REQD
 		FROM
 			MIS.dbo.ST_PROGRAMS_A_136 prog
 		WHERE
 			prog.EFF_TRM_D <> ''
+			AND prog.EFF_TRM_D <= @max_term
 			AND prog.AWD_TY = @awd_type
-			AND prog.END_TRM = ''
+			AND (prog.END_TRM = '' OR prog.END_TRM >= @max_term)
+
 END
 
-SELECT
-	class.crsId
-	,SUM(CASE WHEN class.NON_FF_PERC = 100 THEN 1 ELSE 0 END) AS 'NumOnlineAvailable'
-	,AVG(class.CNTCT_HRS) AS 'HRS'
-INTO
-	#temp
-FROM
-	MIS.dbo.ST_CLASS_A_151 class
-	INNER JOIN MIS.dbo.ST_COURSE_A_150 course ON class.crsId = course.CRS_ID
-WHERE
-	class.effTrm > @min_term
-	AND class.effTrm < @max_term
-GROUP BY
-	class.crsId
+INSERT INTO #programcourserequirements
+	SELECT
+		p.PGM_CD
+		,prog.CRS_ID_X
+		,MAX((course.MIN_CNTCT_HRS + course.MAX_CNTCT_HRS) / 2.0) AS 'HRS'
+		,'' AS 'OfferedOnline'
+	FROM
+		#programs p
+		INNER JOIN MIS.dbo.ST_PROGRAMS_A_136 prog ON prog.PGM_CD_X = p.PGM_CD
+		INNER JOIN MIS.dbo.ST_COURSE_A_150 course ON prog.CRS_ID_X = course.CRS_ID
+	WHERE
+		prog.CRS_ID_X <> ''
+	GROUP BY
+		p.PGM_CD
+		,prog.CRS_ID_X
 
+UPDATE #programcourserequirements
+	SET OfferedOnline = (
+		SELECT
+			CASE
+				WHEN SUM(CASE WHEN class.NON_FF_PERC <= 100 AND class.NON_FF_PERC >= 50 THEN 1 ELSE 0 END) > 0 THEN 'Y'
+				ELSE 'N'
+			END
+		FROM
+			#programcourserequirements p
+			INNER JOIN MIS.dbo.ST_CLASS_A_151 class ON class.crsId = p.CRS_ID
+		WHERE
+			class.effTrm <= @max_term
+			AND class.effTrm >= @min_term
+			AND class.CLS_STAT <> 'C'
+			AND class.crsId = procourse.CRS_ID
+		GROUP BY
+			p.CRS_ID
+	)
+	FROM
+		#programcourserequirements procourse
 
 SELECT
 	@min_term
 	,@max_term
-	,p.AWD_TY
-	,p.PGM_CD
-	,CASE WHEN prog.PGM_OFFCL_TTL <> '' THEN prog.PGM_OFFCL_TTL ELSE prog.PGM_TRK_TTL END
-	,AVG(prog.PGM_TTL_MIN_CNTCT_HRS_REQD)
-	,CASE WHEN SUM(CASE WHEN t.NumOnlineAvailable > 0 THEN t.HRS ELSE 0 END) > AVG(prog.PGM_TTL_MIN_CNTCT_HRS_REQD) THEN AVG(prog.PGM_TTL_MIN_CNTCT_HRS_REQD) ELSE SUM(CASE WHEN t.NumOnlineAvailable > 0 THEN t.HRS ELSE 0 END) END
+	,p1.PGM_CD
+	,p2.AWD_TY
+	,''
+	,MIN(p2.HRS_REQD)
+	,CASE
+		WHEN SUM(CASE WHEN p1.OfferedOnline = 'Y' THEN CAST(p1.HRS AS FLOAT) ELSE 0.0 END) > MIN(p2.HRS_REQD) THEN MIN(p2.HRS_REQD)
+		ELSE SUM(CASE WHEN p1.OfferedOnline = 'Y' THEN CAST(p1.HRS AS FLOAT) ELSE 0.0 END)
+	END
+	,(SUM(CASE WHEN p1.OfferedOnline = 'Y' THEN CAST(p1.HRS AS FLOAT) ELSE 0.0 END) / MIN(p2.HRS_REQD)) * 100
 FROM
-	#programs p
-	INNER JOIN MIS.dbo.ST_PROGRAMS_A_136 prog ON prog.PGM_CD = p.PGM_CD
-											AND prog.EFF_TRM_D <> ''
-											AND prog.END_TRM = ''
-	INNER JOIN MIS.dbo.ST_PROGRAMS_A_136 progreq ON progreq.PGM_CD_X = p.PGM_CD
-	INNER JOIN #temp t ON t.crsId = progreq.CRS_ID_X
+	#programcourserequirements p1
+	INNER JOIN #programs p2 ON p2.PGM_CD = p1.PGM_CD
 GROUP BY
-	p.PGM_CD
-	,p.AWD_TY
-	,prog.PGM_OFFCL_TTL
-	,prog.PGM_TRK_TTL
+	p1.PGM_CD
+	,p2.AWD_TY
+HAVING
+	SUM(CASE WHEN p1.OfferedOnline = 'Y' THEN CAST(p1.HRS AS FLOAT) ELSE 0.0 END) / AVG(CAST(p2.HRS_REQD AS FLOAT)) >= .125
